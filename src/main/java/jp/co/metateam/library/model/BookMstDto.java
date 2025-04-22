36aebbdbeaba84a68b1f53ee7df6fe831256a714
ririka-sonoda-mt <ririka.sonoda@metateam.co.jp>
2025-04-22 17:38:51 +0900

    コミットメッセージ

--- src/main/java/jp/co/metateam/library/model/BookMstDto.java
+++ src/main/java/jp/co/metateam/library/model/BookMstDto.java
@@ -3,6 +3,7 @@ package jp.co.metateam.library.model;
 import java.security.Timestamp;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,12 +15,19 @@ import lombok.Setter;
 @Setter
 public class BookMstDto {
     
+    
     private Long id; 
     
+    @NotEmpty(message = "ISBNは必須です")
+    @Size(max =13,min = 13, message="ISBNは13桁以上で入力してください")
+    @Pattern(regexp = "^[0-9]+$", message = "ISBNは半角数字で入力してください")
     private String isbn;
 
+    @NotEmpty(message = "書籍名は必須です")
+    @Size(max = 255, message="書籍名は255文字以上で入力してください")
+
     private String title;
-    
+
     private Timestamp deletedAt;
 
     private BookMst bookMst;
